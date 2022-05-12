d228106f4198fc4a51587b541fa9aace257b6097
MOK <apisit@gmail.com>
2022-05-13 03:27:53 +0700

    edit login required

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     path('', main, name = 'main'),
-    path('home/', home, name="home"),
+    path('home/', login_required(home,login_url='login'), name="home"),
     path('add_post/', login_required(addpost, login_url='login'), name="add-post"),
 
     path('article/<int:id>', login_required(postdetail, login_url='login'), name="article-detail"),
